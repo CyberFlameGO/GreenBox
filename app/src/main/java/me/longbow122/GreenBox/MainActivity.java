c0f0e3d31b6590582c8a0a5bc794d3e32ccf455e
Longbow <longbow@dyescape.com>
2022-02-28 11:36:21 +0000

    The original program upload. More will be added as the program progresses. GUI reworks are to come.

---
+++ app/src/main/java/me/longbow122/GreenBox/MainActivity.java
@@ -0,0 +1,32 @@
+package me.longbow122.GreenBox;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView scanImage = findViewById(R.id.scanRFIDButton);
+        message = findViewById(R.id.testString);
+
+        // Attach a click listener for the scan image.
+        scanImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickScan();
+            }
+        });
+    }
+
+    private void clickScan() {
+        message.setText("lmao");
+        System.out.println("Scanner for distance was clicked!");
+    }
+}
